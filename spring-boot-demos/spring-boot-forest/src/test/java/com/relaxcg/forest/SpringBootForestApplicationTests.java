ed03514f5f9339932f835da651fbc51287a51461
relaxcg <gaocheng@yingzi.com>
2023-11-20 09:21:23 +0800

    replenish code

---
+++ spring-boot-demos/spring-boot-forest/src/test/java/com/relaxcg/forest/SpringBootForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relaxcg.forest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
